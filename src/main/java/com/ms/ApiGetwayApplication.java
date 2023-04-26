92f274aa70109ff794859c60b45ef6bbeadaa4dc
raosahebDmk <raosaheb.deshmukh88@gmail.com>
2023-04-26 14:59:12 +0530

    added initial and apigateway code

---
+++ src/main/java/com/ms/ApiGetwayApplication.java
@@ -0,0 +1,13 @@
+package com.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetwayApplication.class, args);
+	}
+
+}
